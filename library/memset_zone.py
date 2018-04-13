16a2a02af2717205506350e694442e61d094bdf4
Simon Weald <simon@simonweald.com>
2018-04-13 19:59:06 +0100

    missed payload dict instantiation

--- library/memset_zone.py
+++ library/memset_zone.py
@@ -83,6 +83,7 @@ def create_or_delete(args):
     has_changed = False
     msg = ''
     api_method = 'dns.zone_list'
+    payload = args['payload']
 
     zone_exists = check_zone(api_key=args['api_key'], api_method=api_method, name=args['name'], payload=args['payload'])
 
