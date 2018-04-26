bc7230a392a21cfb01e7eafd5e0ecc8e026052de
Simon Weald <simon@simonweald.com>
2018-04-26 22:36:58 +0100

    Reworked entire module's structure:  * renamed zone_name to zone - easier to understand  * added return documentation  * improved performance by removing redundant API calls  * change some variable names to make their purpose more obvious  * made absent state idempotent (it returned failure when deleting a    non-existent domain - should return with no change)  * return domain info from the API where appropriate

--- lib/ansible/modules/cloud/memset/memset_zone_domain.py
+++ lib/ansible/modules/cloud/memset/memset_zone_domain.py
@@ -35,7 +35,7 @@ options:
         aliases: ['name']
         description:
             - The zone domain name. Ensure this value has at most 250 characters.
-    zone_name:
+    zone:
         required: true
         description:
             - The zone to add the domain to (this must already exist)
@@ -52,48 +52,66 @@ EXAMPLES = '''
     api_key: 5eb86c9196ab03919abcf03857163741
 '''
 
-RETURN = ''' # '''
+RETURN = '''
+memset_api:
+  description: Domain info from the Memset API
+  returned: when state == present and Gcheck_mode
+  type: complex
+  contains:
+    domain:
+      description: Domain name
+      returned: always
+      type: string
+      sample: "example.com"
+    id:
+      description: Domain ID
+      returned: always
+      type: string
+      sample: "b0bb1ce851aeea6feeb2dc32fe83bf9c"
+'''
 
 
-def check(args):
+def check(args, retvals=dict()):
     api_method = 'dns.zone_domain_list'
+    has_changed = False
 
-    _, _, response = memset_api_call(api_key=args['api_key'], api_method=api_method, payload=['payload'])
+    has_failed, msg, response = memset_api_call(api_key=args['api_key'], api_method=api_method)
 
-    zone_exists = check_zone_domain(data=response, domain=args['domain'])
+    domain_exists = check_zone_domain(data=response, domain=args['domain'])
 
     # set changed to true if the operation would cause a change
-    has_changed = ((zone_exists and args['state'] == 'absent') or (not zone_exists and args['state'] == 'present'))
+    has_changed = ((domain_exists and args['state'] == 'absent') or (not domain_exists and args['state'] == 'present'))
 
-    module.exit_json(changed=has_changed)
+    retvals['changed'] = has_changed
+    retvals['failed'] = has_failed
+
+    return(retvals)
 
 
-def create_or_delete_domain(args):
-    has_changed = False
-    has_failed = False
-    msg, _stderr = None
-    payload = args['payload']
+def create_or_delete_domain(args, retvals=dict()):
+    has_changed, has_failed = False, False
+    msg, _stderr, _memset_api = None, None, None
+    payload = dict()
 
     # get the zones and check if the relevant zone exists
     api_method = 'dns.zone_list'
     _, _, response = memset_api_call(api_key=args['api_key'], api_method=api_method)
 
-    zone_exists = check_zone(data=response, name=args['zone_name'])
+    zone_exists = check_zone(data=response, name=args['zone'])
+    if not zone_exists:
+        has_failed = True
+        _stderr = "DNS zone '{}' does not exist, cannot create domain." . format(args['zone'])
+        module.fail_json(failed=has_failed, msg=_stderr, stderr=_stderr)
 
     if args['state'] == 'present':
-        if args['zone_name'] is None:
-            _stderr = 'A zone is needed to add the domain to.'
-            module.fail_json(failed=True, stderr=_stderr)
-        api_method = 'dns.zone_list'
-        _, _, response = memset_api_call(api_key=args['api_key'], api_method=api_method, payload=payload)
         counter = 0
         for zone in response.json():
-            if zone['nickname'] == args['zone_name']:
+            if zone['nickname'] == args['zone']:
                 zone_id = zone['id']
                 counter += 1
         if counter == 1:
             api_method = 'dns.zone_domain_list'
-            _, _, response = memset_api_call(api_key=args['api_key'], api_method=api_method, payload=payload)
+            _, _, response = memset_api_call(api_key=args['api_key'], api_method=api_method)
             for zone_domain in response.json():
                 if zone_domain['domain'] == args['domain']:
                     has_changed = False
@@ -102,57 +120,73 @@ def create_or_delete_domain(args):
                 api_method = 'dns.zone_domain_create'
                 payload['domain'] = args['domain']
                 payload['zone_id'] = zone_id
-                has_failed, msg, response = memset_api_call(api_key=args['api_key'], api_method=api_method, payload=payload)
+                has_failed, _msg, response = memset_api_call(api_key=args['api_key'], api_method=api_method, payload=payload)
                 if not has_failed:
                     has_changed = True
+                else:
+                    msg = _msg
         else:
             has_failed = True
             _stderr = 'Multiple zones with the same name exist.'
-            module.fail_json(failed=True, stderr=_stderr)
+            module.fail_json(failed=True, msg=_stderr, stderr=_stderr)
     if args['state'] == 'absent':
-        if zone_exists:
+        api_method = 'dns.zone_domain_list'
+        _, _, response = memset_api_call(api_key=args['api_key'], api_method=api_method)
+        domain_exists = check_zone_domain(data=response, domain=args['domain'])
+        if domain_exists:
             api_method = 'dns.zone_domain_delete'
             payload['domain'] = args['domain']
             has_failed, msg, response = memset_api_call(api_key=args['api_key'], api_method=api_method, payload=payload)
             if not has_failed:
                 has_changed = True
+                _memset_api = response.json()
 
-    if has_failed:
-        module.fail_json(failed=True, msg=msg)
+    retvals['changed'] = has_changed
+    retvals['failed'] = has_failed
+    retvals['msg'] = msg
+    if _memset_api is not None:
+        retvals['memset_api'] = _memset_api
 
-    if has_changed:
-        module.exit_json(changed=True, msg=msg)
-    else:
-        module.exit_json(changed=False, msg=msg)
+    return(retvals)
 
-
-def main():
+def main(args= dict()):
     global module
     module = AnsibleModule(
         argument_spec=dict(
             state=dict(required=False, default='present', choices=['present', 'absent'], type='str'),
             api_key=dict(required=True, type='str', no_log=True),
             domain=dict(required=True, aliases=['name'], type='str'),
-            zone_name=dict(required=True, aliases=['zone'], type='str')
+            zone=dict(required=True, type='str')
         ),
         supports_check_mode=True
     )
 
-    args = dict()
-    args['payload'] = dict()
     args['state'] = module.params['state']
     args['api_key'] = module.params['api_key']
     args['domain'] = module.params['domain']
-    args['zone_name'] = module.params['zone_name']
+    args['zone'] = module.params['zone']
 
     # zone domain length must be less than 250 chars
     if len(args['domain']) > 250:
-        module.fail_json(failed=True, stderr="Zone domain must be less than 250 characters in length.")
+        _stderr = 'Zone domain must be less than 250 characters in length.'
+        module.fail_json(failed=True, msg = _stderr, stderr=_stderr)
 
     if module.check_mode:
-        check(args)
+        retvals = check(args)
     else:
-        create_or_delete_domain(args)
+        retvals = create_or_delete_domain(args)
+
+    if args['state'] == 'present' and not module.check_mode and retvals['changed'] and not retvals['failed']:
+        payload = dict()
+        payload['domain'] = args['domain']
+        api_method = 'dns.zone_domain_info'
+        _, _, response = memset_api_call(api_key=args['api_key'], api_method=api_method, payload=payload)
+        retvals['memset_api'] = response.json()
+
+    if retvals['failed']:
+        module.fail_json(**retvals)
+    else:
+        module.exit_json(**retvals)
 
 from ansible.module_utils.basic import AnsibleModule
 
